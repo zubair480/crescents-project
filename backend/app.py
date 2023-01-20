b2ef7645f8c142e30af7e7d2ea6d273c4b4df049
Zubair Zafar <zubairzafar480@gmail.com>
2023-01-21 01:42:05 +0500

    third commit

--- backend/app.py
+++ backend/app.py
@@ -8,53 +8,68 @@ api_key='NNmLnS0p097GAVedJjOzMu4SoxTUuD90'
 url = "https://api.ai21.com/studio/v1/j1-grande/complete"
 app = Flask(__name__)
 
+
+def get_data(text):
+    
+    payload={
+            "prompt": str("Q:"+text),
+            "numResults": 1,
+            "maxTokens": 40,
+            "temperature": 0,
+            "topKReturn": 0,
+            "topP":1,
+            "countPenalty": {
+                "scale": 0,
+                "applyToNumbers": False,
+                "applyToPunctuations": False,
+                "applyToStopwords": False,
+                "applyToWhitespaces": False,
+                "applyToEmojis": False
+            },
+            "frequencyPenalty": {
+                "scale": 0,
+                "applyToNumbers": False,
+                "applyToPunctuations": False,
+                "applyToStopwords": False,
+                "applyToWhitespaces": False,
+                "applyToEmojis": False
+            },
+            "presencePenalty": {
+                "scale": 0,
+                "applyToNumbers": False,
+                "applyToPunctuations": False,
+                "applyToStopwords": False,
+                "applyToWhitespaces": False,
+                "applyToEmojis": False
+        },
+        "stopSequences":["Q:"]
+        }
+    
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer NNmLnS0p097GAVedJjOzMu4SoxTUuD90"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    print(data)
+    return data['completions'][0]['data']['text']
 @app.route('/',methods=['GET','POST'])
 def index():
-    response = requests.post(url, json=payload, headers=headers)
+    data=''
     #  print(response.text)
     if request.method == 'POST':
         print("text =", request.form['user_text'])
-    data = response.text
+        text=request.form['user_text']
+        data=get_data(text)
+        
+    
     return render_template('index.html',data=data)
 
 
-payload = {"prompt": 'what is life?',
-    "numResults": 1,
-    "maxTokens": 16,
-    "minTokens": 0,
-    "temperature": 0.7,
-    "topP": 1,
-    "topKReturn": 0,
-    "frequencyPenalty": {
-        "scale": 1,
-        "applyToWhitespaces": True,
-        "applyToPunctuations": True,
-        "applyToNumbers": True,
-        "applyToStopwords": True,
-        "applyToEmojis": True
-    },
-    "presencePenalty": {
-        "scale": 0,
-        "applyToWhitespaces": True,
-        "applyToPunctuations": True,
-        "applyToNumbers": True,
-        "applyToStopwords": True,
-        "applyToEmojis": True
-    },
-    "countPenalty": {
-        "scale": 0,
-        "applyToWhitespaces": True,
-        "applyToPunctuations": True,
-        "applyToNumbers": True,
-        "applyToStopwords": True,
-        "applyToEmojis": True
-    }
-}
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "Authorization": "Bearer NNmLnS0p097GAVedJjOzMu4SoxTUuD90"
-}
+
 
 if __name__ == '__main__':
      app.run()
+     
+ 
